391ff261a94a00fb137dfbf8016b89ebdcec0da2
DawiT <Dawid@Tararuj.be>
2021-01-26 14:16:01 +0100

    fin

---
+++ main.js
@@ -0,0 +1,11 @@
+const pcTartine = document.getElementsByClassName('tartine');
+
+console.log(pcTartine[0].innerText); 
+
+const toast = document.getElementById('toast');
+
+console.log(toast.innerText);
+
+// diff: c'est plus un tableau mais elem unique;)
+
+console.log(pcTartine[pcTartine.length-1]); 
